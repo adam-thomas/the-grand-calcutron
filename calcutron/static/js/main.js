2d26003deb48a9dbf7d8b06647f5052ed820ce52
Adam Thomas <adamthomas@e3d-online.com>
2019-11-05 22:05:14 +0000

    Nicer show/hide behaviour.

--- calcutron/static/js/main.js
+++ calcutron/static/js/main.js
@@ -1,21 +1,32 @@
-function showAddInner(children_element) {
-    title_input_element = children_element.children(".task-form-wrapper").find(".task-title");
-
+function showAddInner(children_element, main_row_element) {
+    main_row_element.children(".show-add-form").addClass("show");
+    main_row_element.children(".delete-task").addClass("show");
     children_element.addClass("show");
-    title_input_element.focus();
 }
 
 
 function toggleChildren(button_element, task_id) {
     children_element = $("#children-of-" + task_id);
+    main_row_element = $(button_element).parent();
 
     if (children_element.hasClass("show")) {
         children_element.removeClass("show");
+        children_element.children(".task-form-wrapper").removeClass("show");
+        main_row_element.children(".show-add-form").removeClass("show");
+        main_row_element.children(".delete-task").removeClass("show");
         $(button_element).html("v");
     } else {
-        showAddInner(children_element);
+        showAddInner(children_element, main_row_element);
         $(button_element).html("^");
     }
+}
+
+
+function showAddForm(task_id) {
+    form_wrapper_element = $("#add-form-of-" + task_id);
+    form_wrapper_element.addClass("show");
+    title_input_element = form_wrapper_element.find(".task-title");
+    title_input_element.focus();
 }
 
 
@@ -47,9 +58,11 @@ function submitAjaxForm(event, form_element, target_url) {
     data = parseForm(form_element);
 
     $.post(target_url, data, (return_data, status) => {
-        siblings_list_id = "#children-of-" + data.parent;
-        $(siblings_list_id).replaceWith(return_data);
-        showAddInner($(siblings_list_id));
+        children_element = $("#children-of-" + data.parent);
+        children_element.replaceWith(return_data);
+
+        showAddInner(children_element, children_element.parent().children(".main-row"));
+        showAddForm(data.parent);
     });
 }
 
