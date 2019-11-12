558fa78ab186d7692d5aa408da15406112957197
Adam Thomas <adamthomas@e3d-online.com>
2019-11-12 22:01:45 +0000

    Make it work, and all that.

---
+++ react/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (<h1>"hooray it worked!"</h1>);
+    }
+}
+
+const wrapper = document.getElementById("app");
+if (wrapper) {
+    ReactDOM.render(<h1>"hooray it worked!"</h1>, wrapper);
+}
