bc7c8ee02a32ec93cc6dbf45455999012c047a94
Adam Thomas <adamthomas@e3d-online.com>
2019-11-22 15:41:18 +0000

    Default DEBUG to True again. I think I messed it up last time.

--- calcutron/settings.py
+++ calcutron/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = ['*'] if DEBUG else ["the-grand-calcutron.herokuapp.com"]
 BASE_URL = "https://the-grand-calcutron.herokuapp.com"
