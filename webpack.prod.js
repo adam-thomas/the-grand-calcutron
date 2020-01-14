1a68c9be568c21eb47242ff20f5e5cfbe6c6e1e6
Adam Thomas <adamthomas@e3d-online.com>
2020-01-14 20:54:04 +0000

    Set up loaders properly for production. Oops.

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,17 @@ module.exports = {
         },
         {
           test: /\.less$/,
-          loader: 'less-loader',
+          use: [
+            {
+              loader: 'style-loader', // creates style nodes from JS strings
+            },
+            {
+              loader: 'css-loader', // translates CSS into CommonJS
+            },
+            {
+              loader: 'less-loader', // compiles Less to CSS
+            },
+          ],
         },
       ]
     },
