6ed6791d113a016b6b98df9cf5391f342d4e6b8e
adam-thomas <sortoflikechess@gmail.com>
2026-02-10 15:56:36 +0000

    Fix up CSRF handling and empty responses.

--- react/ajax_requests.js
+++ react/ajax_requests.js
@@ -9,8 +9,30 @@ function _run_fetch(url, options, success_callback) {
     // Set up JSON parsing and success and failure handling. All errors are reported
     // in basic fashion, using an `alert`, and re-thrown for other Promises to
     // potentially catch later.
+    options = {
+        ...options,
+        headers: {
+            ...(options.headers || {}),
+            "X-CSRFToken": taskState.csrf,
+            "Content-Type": "application/json",
+        },
+    };
+
+    if (options.body) {
+        options.body = JSON.stringify(options.body);
+    }
+
     return fetch(url, options).then(
         (response) => {
+            // Empty responses will throw an error if we try to call response.json() on them.
+            // We can check for expected empty responses by testing the request method.
+            // Clunky, but at least it also gives us protection against unexpected empty
+            // responses to GET/POST/etc requests.
+            if (options.method == "DELETE") {
+                success_callback();
+                return;
+            }
+
             return response.json().then(
                 (json_data) => {
                     if (json_data.errors) {
@@ -42,21 +64,21 @@ function get(url, success_callback) {
 function post(url, data, success_callback) {
     return _run_fetch(url, {
         method: "POST",
-        body: _add_csrf(data),
+        body: data,
     }, success_callback);
 }
 
 function patch(url, data, success_callback) {
     return _run_fetch(url, {
         method: "PATCH",
-        body: _add_csrf(data),
+        body: data,
     }, success_callback);
 }
 
 function remove(url, data, success_callback) {
     return _run_fetch(url, {
         method: "DELETE",
-        body: _add_csrf(data),
+        body: data,
     }, success_callback);
 }
 
